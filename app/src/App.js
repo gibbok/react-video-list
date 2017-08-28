d55f21446457c2b4ab2f68b4f76fdb7175f89db7
GibboK <gibbok.coding@gmail.com>
2017-08-28 08:19:25 +0200

    Add data videos to App

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import VideoList from './VideoList.js'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      ui: {},
+      data: {
+        'kJQP7kiw5Fk':{
+          title: 'Luis Fonsi - Despacito ft. Daddy Yankee',
+          views: 3446728587
+        },
+        'RgKAFK5djSk':{
+          title: 'Wiz Khalifa - See You Again ft. Charlie Puth [Official Video] Furious 7 Soundtrack',
+          views: 3065485805  
+        },
+        '9bZkp7q19f0':{
+          title: 'PSY - GANGNAM STYLE(강남스타일) M/V',
+          views: 2941023973  
+        }
+      }
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +34,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <VideoList />
       </div>
     );
   }
---
+++ app/src/VideoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class VideoList extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>{'VideoList'}</div>
+        );
+    }
+};
+
+export default VideoList;
